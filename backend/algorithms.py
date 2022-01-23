fb26de874ac7ef14891c9c810973cfec2e4eb26a
Floris Schmidt <floris.schmidt@hotmail.com>
2022-01-23 20:16:45 +0100

    ADDED succesive_elimination

--- backend/algorithms.py
+++ backend/algorithms.py
@@ -1,15 +1,16 @@
-from scipy.stats import f_oneway
+from data_gen import Website
+from typing import List
+from math import sqrt, log, pow
 
-def run_test(data, budget, algorithm):
+def run_test(website_list: List(Website), budget, algorithm, accuracy):
     #1 Unpack data 
     #2 Pass to right test
-    unpacked_data = data
-    if algorithm == "mv":
-        results = standard_multivariate_test(unpacked_data, budget)
+    if algorithm == "se":
+        results = succesive_elimination(website_list, budget, accuracy)
     elif algorithm == 'thompson':
-        results = thompson_sampling(unpacked_data, budget)
+        results = thompson_sampling(website_list, budget)
     elif algorithm == 'epsilon_greedy':
-        results = epsilon_greedy(unpacked_data, budget)
+        results = epsilon_greedy(website_list, budget)
     return results
 
 def standard_multivariate_test(data, budget):
@@ -23,6 +24,35 @@ def thompson_sampling(data, budget):
 def epsilon_greedy(data, budget):
     pass
 
+
+def succesive_elimination(website_list, budget, accuracy):
+    t = 1
+    S = website_list
+    c = 4
+    delta = 1 - accuracy
+
+    while (len(S) > 1) and (budget-t>0):
+        for site in S:
+            site.pull()
+
+
+        p_a = [site.average for site in S]
+        p_max = max(p_a)
+        n = len(p_a)
+        alpha_t = sqrt(log((c*n*pow(t,2))/delta)/t)
+
+        for a in S:
+            if(p_max - a.average>=2*alpha_t):
+                S.remove(a)
+        t+=1
+
+    if len(S)==1:
+        print(f'Best website found with {S[0].name}')
+    else:
+        print(f'Not enough budget to find difference between sites')
+        for site in S:
+            print(site.name)
+    return S
 
 ### HELPER FUNCTIONS ###
 

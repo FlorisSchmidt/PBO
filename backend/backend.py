7d2a7bd839b2913b4027f0df6fd2b54f9ed4cf26
Floris Schmidt <floris.schmidt@hotmail.com>
2022-01-27 15:25:08 +0100

    return gen data

--- backend/backend.py
+++ backend/backend.py
@@ -3,7 +3,7 @@ from . import data_gen
 
 def run_experiment(website_list, budget, accuracy):
     website_data = data_gen.generate_data(budget, website_list, 0.1, 0.7, 0.2)
-    return algorithms.succesive_elimination(list(website_data.values()), budget, accuracy)
+    return algorithms.succesive_elimination(list(website_data.values()), budget, accuracy), website_data
 
 if __name__ == '__main__':
     pass

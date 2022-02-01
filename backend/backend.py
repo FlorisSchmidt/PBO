e64235866d920249e9895b8d7d04046564f9872c
Floris Schmidt <floris.schmidt@hotmail.com>
2022-02-01 21:10:50 +0100

    Added analysis of SE

--- backend/backend.py
+++ backend/backend.py
@@ -2,7 +2,7 @@ from . import algorithms
 from . import data_gen
 
 def run_experiment(website_list, budget, accuracy):
-    website_data = data_gen.generate_data(budget, website_list, 0.1, 0.7, 0.2)
+    website_data = data_gen.generate_data(budget, website_list, 0.1, 0.3, 0.2)
     return algorithms.succesive_elimination(list(website_data.values()), budget, accuracy), website_data
 
 if __name__ == '__main__':
